89b492eb34c494b08e40a2423abcc0672220c7f9
IKai-Lai <58580854+IKai-Lai@users.noreply.github.com>
2023-12-06 00:32:26 +0800

    add feature selection and update data_helpers

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from data_helpers import DataHolder
+from preprocessing_pca import fit_pca
 
 data = DataHolder()
-train_datasets, validation_datasets, test_datasets, expected_RUL_datasets = data.get()
+train_datasets, test_datasets, expected_RUL_datasets = data.get()
 
 print(train_datasets[0])
