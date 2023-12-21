273082f08bb4f55563afacae207ff7cbcedc994e
rqhong <r12725058@ntu.edu.tw>
2023-12-21 17:10:42 +0800

    fix: preprocessing method

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from data_helpers import DataHolder
 from configuration import Configuration
 from train import Trainer
-from utils import save, load, plot_prediction, plot_loss
+from utils import set_seeds, save, load, plot_prediction, plot_loss, calculate_RMSE, calculate_MAPE
 from lstm import LSTMModel
 
 def main():
     print("="*80)
-
-    config_main = Configuration(preprocessing_method='pca') # en
+    
+    set_seeds(42)
+    config_main = Configuration(preprocessing_method='none') # pca # en # none
     holder = DataHolder(config_main)
 
     train_dataset, eval_dataset, test_dataset = holder.get(
-        dataset_index = 1,
+        dataset_index = 0,
     )
-    
+
     # Initialize Trainer with the configuration and datasets
+    print("="*80)
     trainer = Trainer(
         config = config_main,
         train_dataset = train_dataset,
@@ -35,7 +37,9 @@ def main():
   
     y_pred, y_true = trainer.predict(forecast_model)
     plot_prediction(y_pred, y_true)
-
+    calculate_RMSE(y_pred, y_true)
+    calculate_MAPE(y_pred, y_true)
+    
 if __name__=="__main__":
     main()
     print("Done!")
